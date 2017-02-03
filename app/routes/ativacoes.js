435d71f7dc9dcd434b86b73e3cf495d1dc4b5750
Camila Corrêa <Camila Corra>
2017-02-02 23:34:28 -0200

    Consultando a base com sql server

--- app/routes/ativacoes.js
+++ app/routes/ativacoes.js
@@ -5,8 +5,31 @@ module.exports = function(app){
 /*Define o comportamento do acesso a rota ativações por get*/
 app.get('/ativacoes', function(request, response){
 
+var sql = require('mssql');
+var config = {
+user:'eduardo',
+password:'santos,1722',
+server:'sqlweb.campanhaturbinesuasvendas.com.br',
+database:'turbine5_dev'};
+
+sql.connect(config,function(err){
+
+if(err) console.log(err);
+
+var request = new sql.Request();
+request.query('select SUM(PONTOS) AS PONTOS from TB_ATIVACOES',function(err,recordset){
+
+if(err) console.log(err);
+
+response.send(recordset);
+
+});
+});
+
 /*Define a view irá renderizar a resposta*/
-response.render("ativacoes/lista");
+
+
+//response.render("ativacoes/lista");
 });
 
 }
